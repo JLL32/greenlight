badeeb287832eedf3c00fdc98e8dc060e7579ed1
JALAL <eabduljalil@outlook.com>
2025-01-23 13:43:18 +0100

    override httprouter special routes to return json

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -16,6 +16,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	id, err := app.readIDParam(r)
 	if err != nil {
 		app.notFoundResponse(w, r)
+		return
 	}
 
 	movie := data.Movie{
